2f8c1a8e648a4c90e710c35b8bb9d131e05f782f
MTahaSh <tahashayk2k@gmail.com>
2023-08-31 14:13:14 +0500

    update code

---
+++ app/components/index.ts
@@ -0,0 +1,24 @@
+import Hero from "./Hero";
+import CusButton from "./CusButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import SearchBar from "./SearchBar"
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CusButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+
+}
